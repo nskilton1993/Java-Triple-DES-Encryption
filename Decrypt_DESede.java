701a5b9ca0dfc08c950bed8208833ac3b015e771
Nicholas Skilton <nickskilton@yahoo.com>
2025-06-16 13:17:33 -0500

    Add files via upload

---
+++ Decrypt_DESede.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+public class Decrypt_DESede {
+
+	public static void main(String[ ] args) {
+		
+		if (args.length != 3) {
+			System.err.println("Usage: java FileDecryptor infile outfile 24CharPassword");  return;
+		}
+		
+		String infile = args[0];
+		String outfile = args[1];
+		String password = args[2];
+		
+		if (password.length() != 24 ) 
+			System.err.println("Password must be 24 characters long");
+		try {
+			FileInputStream fin = new FileInputStream(infile);
+			FileOutputStream fout = new FileOutputStream(outfile);
+			
+			// First create a key specification from the password, then the key.
+			// Note DESede expects 3 keys of 8 bytes each, a total of 24 bytes.
+			byte[ ] desedeKeyData = password.getBytes( );
+			DESedeKeySpec desedeKeySpec =
+					new DESedeKeySpec(desedeKeyData);
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DESede");
+			SecretKey desedeKey = keyFactory.generateSecret(desedeKeySpec);  //****
+			
+			// Read the initialization vector
+			DataInputStream din = new DataInputStream(fin);
+			int ivSize = din.readInt( );  // Retrieve initialization vector.
+			byte[ ] iv = new byte[ivSize];
+			din.readFully(iv);
+			IvParameterSpec ivps = new IvParameterSpec(iv);
+			
+			// Use Triple DES (DESede) - Data Encryption Standard
+			Cipher desede = Cipher.getInstance("DESede/CBC/PKCS5Padding");
+			desede.init(Cipher.DECRYPT_MODE, desedeKey, ivps);
+			
+			byte[ ] input = new byte[64];
+			while (true) {
+				int bytesRead = fin.read(input);
+				if (bytesRead == -1) break;
+				byte[ ] output = desede.update(input, 0, bytesRead);
+				if (output != null) fout.write(output);
+			}
+			
+			byte[ ] output = desede.doFinal( );
+			if (output != null) fout.write(output);
+			fin.close( );
+			fout.flush( );
+			fout.close( );	
+		}
+		catch (InvalidKeySpecException e) {
+			System.err.println(e);
+		}
+		catch (InvalidKeyException e) {
+			System.err.println(e);
+		}
+		catch (InvalidAlgorithmParameterException e) {
+			System.err.println(e);
+		}
+		catch (NoSuchAlgorithmException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		catch (NoSuchPaddingException e) {
+			System.err.println(e);
+		}
+		catch (BadPaddingException e) {
+			System.err.println(e);
+		}
+		catch (IllegalBlockSizeException e) {
+			System.err.println(e);
+		}
+		catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+}
---
+++ Encrypt_DESede.java
@@ -0,0 +1,84 @@
+import java.io.*;            // Based on an example in "Java I/O" by
+import java.security.*;      // Elliotte Harold, O'Reilly, page 244.
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+// Sample usage: C:\> java JavaFileEncryptionDESede roses.plain 
+// A234567890b234567890c234.  The encrypted file will be in roses.plain.desede.
+// Triple DES uses the first 8 bytes of the key to encrypt (56 bits), then the next eight
+// bytes as a key 56 (bits) to decrypt, and finally the last 8 bytes (56 bits) as a key to
+// encrypt.  The result is roughly equivalent to 168 bit encryption.
+
+public class Encrypt_DESede {
+
+	public static void main(String[ ] args) {
+		if (args.length != 2) {
+			System.err.println("Usage: java JavaFileEncryptionDESede filename 24CharPassword");
+			return;
+		}
+		
+		String password = args[1];
+		if (password.length() != 24 )
+			System.err.println("Encryption password must be 24 bytes.");
+		try {
+			FileInputStream fin = new FileInputStream(args[0]);
+			FileOutputStream fout = new FileOutputStream( args[0] + ".desede");
+			
+			// First create a key specification from the password, then the secret key.
+			byte[ ] desedeKeyData = password.getBytes( );
+			DESedeKeySpec desKeySpec = new DESedeKeySpec(desedeKeyData);
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DESede");
+			SecretKey desedeKey = keyFactory.generateSecret(desKeySpec); //***
+			
+			// DES algorithm in CBC mode with PKCS5PPadding and random initialization vector.
+			Cipher desede = Cipher.getInstance("DESede/CBC/PKCS5Padding");
+			
+			// Create initialization vector.
+			desede.init(Cipher.ENCRYPT_MODE, desedeKey); 
+			
+			// Write the length & initialization vector onto the output stream, as plain text.
+			byte[ ] iv = desede.getIV( );
+			DataOutputStream dout = new DataOutputStream(fout);
+			dout.writeInt(iv.length);  // Length of initialization vector, in plain text.
+			dout.write(iv);            // Actual initialization vector, in plain text.
+			
+			//Encrypt 64 byte blocks.  DESede uses 8 bytes (56 bits) at a time.
+			byte[ ] input = new byte[64]; 
+			while (true) {  //The buffer does not have to be a multiple of 8.
+				int bytesRead = fin.read(input);  // Get 64 bytes if possible from plain text.
+				if (bytesRead == -1) break;         // Check EOF.
+				byte[ ] output = desede.update(input, 0, bytesRead); 
+				if (output != null) dout.write(output);  //Write encrypted info to file.
+			}
+			
+			byte[ ] output = desede.doFinal( );  // Pad and flush any remaining data.
+			if (output != null) dout.write(output);  // Write remaining encrypted data to file.
+			fin.close( );
+			dout.flush( );
+			dout.close( );	
+		}
+		catch (InvalidKeySpecException e) {
+			System.err.println(e);
+		}
+		catch (InvalidKeyException e) {
+			System.err.println(e);
+		}
+		catch (NoSuchAlgorithmException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		catch (NoSuchPaddingException e) {
+			System.err.println(e);
+		}
+		catch (BadPaddingException e) {
+			System.err.println(e);
+		}
+		catch (IllegalBlockSizeException e) {
+			System.err.println(e);
+		}
+		catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+}
